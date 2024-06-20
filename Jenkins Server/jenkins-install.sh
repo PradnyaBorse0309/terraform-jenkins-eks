059a294691dca10569ff2dc2c07499e58d19efca
Rahul <rahulshelake3@gmail.com>
2024-06-20 17:59:46 +0530

    Adding files to repo

---
+++ Jenkins Server/jenkins-install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install Jenkins
+sudo yum update -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+    https://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+sudo yum upgrade -y
+sudo amazon-linux-extras install java-openjdk11 -y
+sudo yum install jenkins -y 
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# Then Install Git 
+sudo yum install git -y
+
+# Then Install Terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+sudo yum -y install terraform
+
+# Finall Install kubectl
+sudo curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.23.6/bin/linux/amd64/kubectl
+sudo chmod +x ./kubectl
+sudo mkdir -p $HOME/bin && sudo cp ./kubectl $HOME/bin/kubectl && export PATH=$PATH:$HOME/bin
